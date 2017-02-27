ead5f1e5b1c7d1141223bf23720542080b31cd5e
ern0 <ern0@linkbroker.hu>
2017-02-27 23:04:49 +0100

    help cosmetics

--- youda.py
+++ youda.py
@@ -21,19 +21,19 @@ class Youda(Thread):
 		print("""
 How to setup:
   - download this script to your computer
-  - install youtube-dl 
-    on a Mac: brew install youtube-dl
+  - install youtube-dl <br/>
+    on a Mac: brew install youtube-dl <br/>
     on any OS: sudo pip install --upgrade youtube_dl
-  - install a context-menu extension in your browser
-    e.g. Context Menus for Chrome https://goo.gl/8hgwuB
+  - install a context-menu extension in your browser <br/>
+    e.g. Context Menus for Chrome https://goo.gl/8hgwuB <br/>
   - add a custom action for links, which sends the URL to
-    http://localhost:8012/q=%s
+    http://localhost:8012/q=%s <br/>
     where "%s" is the variable name for the selected URL
 
 How to use:
-  - start this script in a shell window:
+  - start this script in a shell window: <br/>
     youda.py 8009 ~/Downloads/youtube
-  - in your browser, right-click in a YouTube link and
+  - in your browser, right-click in a YouTube link and <br/>
     select custom context menu item you've added
   - this script will catch the URL and call youtube-dl with it
   - do not abort the script until it finishes
